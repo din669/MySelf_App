b114e40f0c7ad7cba857027ce04f0155ed3efa32
din669 <muhammadsyarifuddin669@gmail.com>
2022-05-23 02:00:32 +0700

    update final

---
+++ app/src/main/java/com/example/myselfapp/CostumDialog.java
@@ -0,0 +1,26 @@
+package com.example.myselfapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+//NIM    : 10119216
+//Nama   : Muhammad Syarifuddin Rahiman
+//Kelas  : IF-6
+//Tanggal Pengerjaan : 22-05-2022
+
+public class CostumDialog extends DialogFragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.costum_dialog,container,false);
+
+    }
+}
